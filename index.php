847ccf6c7c008fe74945a806927b74f2222e3bd3
angel-putz <angel.putz@laplateforme.io>
2024-03-04 09:47:29 +0100

    créations des fichiers

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button onclick="window.location.href='minuteur.php'">minuteur</button>
+    <button onclick="window.location.href='chronometre.php'">chronometre</button>
+    <button onclick="window.location.href='horloge.php'">horloge</button>
+    <button onclick="window.location.href='alarme.php'">alarme</button>
+
+</body>
+</html>

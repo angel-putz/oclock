91a417de101fafff240f160557469aa3bf1b15ff
angel-putz <angel.putz@laplateforme.io>
2024-03-07 13:31:39 +0100

    maj

--- alarme/alarme.php
+++ alarme/alarme.php
@@ -7,7 +7,9 @@
     <title>Document</title>
 </head>
 <body>
+    
 <h1>Alarme</h1>
+
 <form action="alarme.php" method="post" id="alarme_1">
     <input type="number" name="heure" id="heure" placeholder="heure">
     <input type="number" name="minutes" id="minutes" placeholder="minutes">
@@ -16,6 +18,7 @@
 </form>
 
 <p> Pour ajouter une alarme, veuillez renseigner l'heure, les minutes et le message de l'alarme.</p>
+
 <form action="alarme.php" method="post" id="alarme_2">
     <input type="number" name="heure" id="heureAjout" placeholder="heure">
     <input type="number" name="minutes" id="minutesAjout" placeholder="minutes">
@@ -24,13 +27,22 @@
 </form>
 
 <p>Alarmes :</p>
+
 <div id="alarme"></div>
+
 <p>Nouvelle alarme :</p>
+
 <div id="nouvelleAlarme"><br></div>
+
 <p>Alarme passé :</p>
+
 <div id="alarmePasse"></div>
+
 <button onclick="window.location.href='../index.php'">Retour</button>
+
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 <script src="./alarme.js"></script>
+
 </body>
 </html>
